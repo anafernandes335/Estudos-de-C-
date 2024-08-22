1127541d5e461bfbaa56581a65637975d3ca03b3
Ana Fernandes <estud.ana24@gmail.com>
2024-08-22 19:37:15 -0300

    Exercício 4
    Questão: 
    Faça um programa que imprima na tela apenas os números ímpares entre 1
    e 50.

---
+++ exercicio4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    for (int num = 1; num <= 50; num++) {
+        if (num % 2 != 0) {
+            cout << num << endl;
+        }
+    }
+
+    return 0;
+}
+
